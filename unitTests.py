066a6ce3cdc32ea1c86375dbbc7948fb96d4f70d
Pawel Motulewski <pawel.motulewski@pragmasoft.pl>
2017-01-05 16:28:23 +0100

    Added Class Casino

--- unitTests.py
+++ unitTests.py
@@ -5,11 +5,12 @@ class TestCards(unittest.TestCase):
     def test_check(self):
         return True
 
+
     def assert_card_value(self, rank, suite, value):
         card = Card(rank, suite)
-        self.assertEqual(card.rank,rank)
-        self.assertEqual(card.suite,suite)
-        self.assertEqual(card.return_value(),value)
+        self.assertEqual(card.rank, rank)
+        self.assertEqual(card.suite, suite)
+        self.assertEqual(card.return_value(), value)
 
     def test_creating_card(self):
         self.assert_card_value('1', 'spades', 1)
@@ -27,21 +28,26 @@ class TestCards(unittest.TestCase):
         self.assert_card_value('K', 'spades', 10)
         self.assert_card_value('A', 'clubs', 11)
 
-
     def test_deck_new_deck(self):
         deck1 = Deck()
         player1 = Player('AAA')
         player2 = Player('BBB')
         player3 = Player('CCC')
-        self.assertEqual(deck1.cards.__len__(),52)
-        card1 = Card('2','clubs')
+        self.assertEqual(deck1.cards.__len__(), 52)
+        card1 = Card('2', 'clubs')
         deck1.add_to_deck(card1)
         self.assertEqual(deck1.cards.__len__(), 53)
-        deck1.give_card_to(card1,player1)
+        deck1.give_card_to(card1, player1)
         self.assertEqual(deck1.cards.__len__(), 52)
         self.assertEqual(player1.cards.__len__(), 1)
-        deck1.give_cards((player2,player3,), 26)
+        deck1.give_cards((player2, player3,), 26)
         self.assertEqual(player2.cards.__len__(), 26)
         self.assertEqual(player2.cards.__len__(), 26)
         self.assertEqual(deck1.cards.__len__(), 0)
 
+    def test_if_class_gracz(self):
+        gracz = Player('TestPlayer')
+        self.assertEqual(gracz.name, 'TestPlayer')
+        self.assertEqual(gracz.cards, [])
+        self.assertEqual(gracz.hit_me, True)
+        self.assertEqual(gracz.in_game, True)
